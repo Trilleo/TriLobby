d4b1b7fddb16f1aa2fb6e0313ab93526b4a1086d
Trilleo <gongsanle@gmail.com>
2026-05-07 20:39:15 +0800

    Feature: Add player effect remove on leave

--- src/main/kotlin/net/trilleo/mc/plugins/trilobby/listeners/player/PlayerLeaveListener.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trilobby/listeners/player/PlayerLeaveListener.kt
@@ -3,10 +3,13 @@ package net.trilleo.mc.plugins.trilobby.listeners.player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.potion.PotionEffectType
 
 class PlayerLeaveListener : Listener {
     @EventHandler
     fun onPlayerLeave(event: PlayerQuitEvent) {
         event.quitMessage(null)
+        event.player.removePotionEffect(PotionEffectType.SPEED)
+        event.player.removePotionEffect(PotionEffectType.SATURATION)
     }
 }
