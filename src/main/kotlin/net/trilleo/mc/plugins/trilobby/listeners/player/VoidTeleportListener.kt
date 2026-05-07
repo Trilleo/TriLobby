2a3a248d863d5fbfb383e0ac57af985cf3d8389b
Trilleo <gongsanle@gmail.com>
2026-05-07 20:32:40 +0800

    Feature: Add teleport message when player fell out of the world

--- src/main/kotlin/net/trilleo/mc/plugins/trilobby/listeners/player/VoidTeleportListener.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trilobby/listeners/player/VoidTeleportListener.kt
@@ -1,5 +1,7 @@
 package net.trilleo.mc.plugins.trilobby.listeners.player
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.trilleo.mc.plugins.trilobby.Main
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -29,5 +31,6 @@ class VoidTeleportListener(private val plugin: JavaPlugin) : Listener {
         if (event.from.y < VOID_Y_THRESHOLD || event.to.y >= VOID_Y_THRESHOLD) return
         val spawn = (plugin as Main).pluginConfig.getServerSpawn() ?: return
         event.player.teleport(spawn)
+        event.player.sendMessage(Component.text("You fell out of the world!").color(NamedTextColor.RED))
     }
 }
